3b50053bc631b8f60401192251fafaf5423837b6
taem <taem@taemui-MacBookAir.local>
2025-09-03 13:52:59 +0900

    .env exmaple

--- util/db.js
+++ util/db.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('memo', 'root', 'qwer1234', {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DB_DATABASE_NAME, process.env.DB_USER_NAME, process.env.DB_USER_PW, {
+  host: process.env.DB_HOST_URI,
+  dialect: process.env.DB_DIALECT
 });
 
 module.exports = sequelize;
